4d23e83bbfb30cf145811cc0a77d040f9ca2d729
Woulei <woulei@me.com>
2017-03-19 12:52:30 +0100

    Added edit and delete buttons to Song show page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,11 @@
 class SongsController < ApplicationController
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to artist_path(@song.artist), notice: "Song deleted."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   resources :artists do
-    resources :songs, only: [:new, :create, :show, :delete]
+    resources :songs, only: [:new, :create, :show, :destroy, :edit]
   end
 end
